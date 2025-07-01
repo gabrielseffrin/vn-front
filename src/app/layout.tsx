f58e5da5dcffc8bae7c156e29867204911d5c7f7
Gabriel Seffrin <gabriel.seffrin@millpar.com>
2025-07-01 16:49:41 -0300

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,9 +10,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="pt-BR">
       <body className="flex h-screen bg-gray-100 text-gray-900">
-        <Sidebar activeMenuItem={''} setActiveMenuItem={function (item: string): void {
-          throw new Error('Function not implemented.')
-        } } />
+        <Sidebar activeMenuItem="" setActiveMenuItem={() => {}} />
         <main className="flex-1 p-6 overflow-auto">{children}</main>
       </body>
     </html>
