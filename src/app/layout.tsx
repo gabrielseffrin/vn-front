c14aee25450dcb80b44596b161b3ef71dfa60c45
Gabriel Seffrin <gabriel.seffrin@millpar.com>
2025-07-01 16:55:52 -0300

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,11 +10,12 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="pt-BR">
       <body className="flex h-screen bg-gray-100 text-gray-900">
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
         <Sidebar 
             activeMenuItem={'relatorio'} 
-            setActiveMenuItem={(item: string) => {
-              console.log('Menu selecionado:', item);
-            }} 
+           setActiveMenuItem={function (item: string): void {
+            throw new Error('Function not implemented.');
+          }} 
           />
         <main className="flex-1 p-6 overflow-auto">{children}</main>
       </body>
